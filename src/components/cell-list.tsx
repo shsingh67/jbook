7fceec3773702c43987302148d5fc8d00634642c
unknown <singhsharandeep676@gmail.com>
2021-05-04 17:18:14 -0700

    added cell state fetching logic

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,8 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+
 const CellList: React.FC = () => {
+  useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+
   return <div>Cell List</div>;
 };
 
