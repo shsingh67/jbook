ee5c84551858cf6c5b69925aebfeb8a46590f7f7
unknown <singhsharandeep676@gmail.com>
2021-05-06 17:24:53 -0700

    cell rendering logic

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,9 +1,15 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
-  useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
 
-  return <div>Cell List</div>;
+  const renderedCells = cells.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+  return <div>{renderedCells}</div>;
 };
 
 export default CellList;
